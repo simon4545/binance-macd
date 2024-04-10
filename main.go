b0c8a58fb76fac2d7d9c403dfed9a241b9281844
simon4545 <simon4545@gmail.com>
2024-04-10 05:29:19 +0000

    asdf

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -27,45 +28,50 @@ func init() {
 
 func main() {
 	GetSymbolInfo(client)
-	// HotList()
-	go CheckCross(client)
+	go HotList()
+	// go CheckCross(client)
 	select {}
 }
 
 func HotList() {
-	url := "https://api.binance.com/api/v3/ticker/24hr"
-	// url := "https://api.binance.com/api/v3/ticker/24hr"
-	response, err := http.Get(url)
-	if err != nil {
-		log.Println("Error making GET request:", err)
-		return
-	}
-	defer response.Body.Close()
-	bodyBytes, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Println("Error reading response body:", err)
-		return
-	}
+	symbols = []string{}
+	if len(config.Symbols) > 0 {
+		symbols = append(symbols, config.Symbols...)
+	} else {
+		url := "https://api.binance.com/api/v3/ticker/24hr"
+		// url := "https://api.binance.com/api/v3/ticker/24hr"
+		response, err := http.Get(url)
+		if err != nil {
+			log.Println("Error making GET request:", err)
+			return
+		}
+		defer response.Body.Close()
+		bodyBytes, err := io.ReadAll(response.Body)
+		if err != nil {
+			log.Println("Error reading response body:", err)
+			return
+		}
 
-	responseBody := string(bodyBytes)
-	value := gjson.Parse(responseBody).Array()
-	for _, symbol := range value {
-		symbolCoin := symbol.Get("symbol").String()
+		responseBody := string(bodyBytes)
+		value := gjson.Parse(responseBody).Array()
+		for _, symbol := range value {
+			symbolCoin := symbol.Get("symbol").String()
 
-		if !strings.HasSuffix(symbolCoin, "USDT") {
-			continue
-		}
-		baseAsset := symbolCoin[:len(symbolCoin)-4]
-		if strings.HasSuffix(baseAsset, "DOWN") || strings.HasSuffix(baseAsset, "UP") {
-			continue
-		}
-		volume24h := symbol.Get("quoteVolume").Float()
-		if len(config.Symbols) > 0 && slices.Contains(config.Symbols, baseAsset) {
-			symbols = append(symbols, baseAsset)
-		}
-		if len(config.Symbols) == 0 {
-			if volume24h > 5_000_000 && !slices.Contains(binanceExcludes, baseAsset) {
+			if !strings.HasSuffix(symbolCoin, "USDT") {
+				continue
+			}
+			baseAsset := symbolCoin[:len(symbolCoin)-4]
+			if strings.HasSuffix(baseAsset, "DOWN") || strings.HasSuffix(baseAsset, "UP") {
+				continue
+			}
+			volume24h := symbol.Get("quoteVolume").Float()
+			if len(config.Symbols) > 0 && slices.Contains(config.Symbols, baseAsset) {
 				symbols = append(symbols, baseAsset)
+			}
+			if len(config.Symbols) == 0 {
+				if volume24h > 5_000_000 && !slices.Contains(binanceExcludes, baseAsset) {
+					symbols = append(symbols, baseAsset)
+				}
 			}
 		}
 	}
@@ -74,5 +80,6 @@ func HotList() {
 	// 	return symbols[i].Percent > symbols[j].Percent
 	// })
 	// symbols = symbols[:100]
+	fmt.Println("总大小", len(symbols))
 	CheckCross(client)
 }
